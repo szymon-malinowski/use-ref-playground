6206c9294ed5d25f9277e13152145eead5ffa9c4
Szymon Malinowski <szmalinowski2014@gmail.com>
2026-06-08 13:51:19 +0200

    add Playground component and update App to include it

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,17 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
+import Playground from "./components/playground";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header></header>
+      <main>
+        <Playground />
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/playground.jsx
@@ -0,0 +1,7 @@
+export default function Playground() {
+  return (
+    <>
+      <h1>useRef Playground</h1>
+    </>
+  );
+}
