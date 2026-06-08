b4ae2f0e8f42b25b12bc45d2e1267daacb95ad12
Szymon Malinowski <szmalinowski2014@gmail.com>
2026-06-08 14:47:42 +0200

    add Playground component with input focus functionality

--- src/components/playground.jsx
+++ src/components/playground.jsx
@@ -1,7 +1,22 @@
+import { useRef } from "react";
+
 export default function Playground() {
+  const inputRef = useRef(null);
+
+  function handleFocus() {
+    // Only focus, if inputRef is not null
+    inputRef.current?.focus();
+    console.log(inputRef.current);
+  }
+
   return (
     <>
       <h1>useRef Playground</h1>
+      <section>
+        <h2>1. Focus on input field</h2>
+        <input type="text" ref={inputRef} />
+        <button onClick={handleFocus}>Focus</button>
+      </section>
     </>
   );
 }
